0a170add9fa6322fcbb073a0e2cc61925a9f0cd4
bwainaina380 <bwainaina380@gmail.com>
2021-04-03 12:05:07 +0300

    Created the layout for the age calculator

--- ageCalculator/app/src/main/java/com/feelgoodapps/myapplication/MainActivity.kt
+++ ageCalculator/app/src/main/java/com/feelgoodapps/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.feelgoodapps.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun calculateAge(view: View) {
+
     }
 }
