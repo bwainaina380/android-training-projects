1c83e85ae2deba7ca8e50236401458264f80443a
bwainaina380 <bwainaina380@gmail.com>
2021-04-10 22:51:52 +0300

    feat: Created the app layout

---
+++ AddDeduct/app/src/main/java/com/feelgoodapps/adddeduct/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.feelgoodapps.adddeduct
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addScore(view: View) {
+
+    }
+
+    fun deductScore(view: View) {
+
+    }
+}
