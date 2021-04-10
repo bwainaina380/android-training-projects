cc1a77831cbe002ba0fc9b0e356707dc73affdf5
bwainaina380 <bwainaina380@gmail.com>
2021-04-10 23:05:51 +0300

    feat: Implemented addScore and deductScore functions

--- AddDeduct/app/src/main/java/com/feelgoodapps/adddeduct/MainActivity.kt
+++ AddDeduct/app/src/main/java/com/feelgoodapps/adddeduct/MainActivity.kt
@@ -2,19 +2,26 @@ package com.feelgoodapps.adddeduct
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun addScore(view: View) {
-
+        var scoreText: TextView = findViewById(R.id.score_display)
+        score++
+        scoreText.setText(score.toString())
     }
 
     fun deductScore(view: View) {
-
+        var scoreText: TextView = findViewById(R.id.score_display)
+        score--
+        scoreText.setText(score.toString())
     }
 }
